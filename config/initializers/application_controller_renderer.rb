9320d5cd75fdeed8ef3f47510c85a484a8ed0cf8
Hiroakikodama <flere2100qp@gmail.com>
2020-03-30 01:49:14 +0900

    Finish 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0d4cab4c3a6d3e3760fd0877d25f59434441216f060e705ca4a10eca7b8e61ad214d4cd33e8c8a9fcc14644871fd177a743bbad64c98600ebc139b8f92fe53e8'
